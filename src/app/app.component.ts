124e20787293a7469d0a1114710b0096524db080
nishant <nish54@gmail.com>
2022-11-24 16:26:43 -0600

    table made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface PatientResponse {
+  Items: Array<PatientData>;
+  Count: number;
+  ScannedCount: number
+}
+
+interface PatientData {
+  bed: number;
+  patient_name: string;
+  status: string;
+  assigned_nurse_id: number;
+  need_urgency: number;
+  need: string;
+  assigned_nurse: string;
+  ID: number;
+  timestamp_created_epoch: number;
+  patient_id: number;
+  timestamp_created: string;
+  room: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +28,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'manik';
+  readonly baseUrl = 'https://8ollpgyfd5.execute-api.us-west-2.amazonaws.com/beta?nurse=Manik';
+  data: Array<PatientData> = [];
+  columns: string[] = [
+    'bed',
+    'patient name',
+    'status',
+    'assigned nurse id',
+    'need urgency',
+    'need',
+    'assigned nurse',
+    'id',
+    'timestamp created epoch',
+    'patient id',
+    'timestamp created',
+    'room'
+  ];
+
+  constructor(private http: HttpClient) {
+  }
+
+
+
+  getData() {
+    return this.http.get<PatientResponse>(this.baseUrl).subscribe(response => this.data = response.Items);
+  }
 }
